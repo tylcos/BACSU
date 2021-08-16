736ba1cbe806c22841b1859535b08727c9e2bb16
tylcos <tylcos@users.noreply.github.com>
2021-08-16 02:33:49 -0400

    Update for new banner

--- Signup.py
+++ Signup.py
@@ -1,57 +1,41 @@
+import sys
+
 import requests
 
-import LoginInfo
 from Parser import Parser
 
 
-settings = {'USE_LOGIN': True,
-            'USERNAME': LoginInfo.username,
-            'PASSWORD': LoginInfo.password,
-            'REQUESTED_CRNs': '84338, 93884, 91445, 89375, 91221, 91200, 91545, 83961, 83660',
-            'SEMESTER': '202008',
+settings = {
+    'REQUESTED_CRNs': '',
+    'SEMESTER': '202108',
+    'SESSID': '',
 
-            # Program settings
-            'BASE_URL': 'https://oscar.gatech.edu/pls/bprod/',
-            'LOGIN_URL': 'https://login.gatech.edu/cas/login?service=https://sso.sis.gatech.edu:443/ssomanager/c/SSB',
-            'LT_ID': '<input type="hidden" name="lt" value="',
-            'PAYLOAD_START': 'term_in=202008&RSTS_IN=DUMMY&assoc_term_in=DUMMY&CRN_IN=DUMMY&start_date_in=DUMMY'
-                             '&end_date_in=DUMMY&SUBJ=DUMMY&CRSE=DUMMY&SEC=DUMMY&LEVL=DUMMY&CRED=DUMMY&GMOD=DUMMY'
-                             '&TITLE=DUMMY&MESG=DUMMY&REG_BTN=DUMMY&MESG=DUMMY&'}
-
+    # Program settings
+    'BASE_URL': 'https://oscar.gatech.edu/pls/bprod/',
+    'LOGIN_URL': 'https://sso.gatech.edu/cas/login',
+}
 
 # Login and request current classes
 s = requests.Session()
 s.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
-response = s.get(settings['LOGIN_URL'])
-
-# Read LT_ID, should change to use BeautifulSoup
-html = str(response.content)
-pos = html.index(settings['LT_ID']) + len(settings['LT_ID'])
-lt = html[pos:html.index('"', pos)]
-
-s.post(settings['LOGIN_URL'],
-       data=dict(username=settings['USERNAME'],
-                 password=settings['PASSWORD'],
-                 lt=lt,
-                 execution='e1s1',
-                 _eventId='submit',
-                 submit='LOGIN'))
-
+s.cookies['SESSID'] = settings['SESSID']
 
 response = s.post(settings['BASE_URL'] + 'bwskfreg.P_AltPin',
-                  data=dict(term_in=settings['SEMESTER']))
-html = str(response.content)
+                  data={
+                      'term_in': settings['SEMESTER']
+                  })
 
-if html.find('<h3>Current Schedule</h3>') == -1:
+responseStr = str(response.content)
+if '<h3>Current Schedule</h3>' not in responseStr:
     raise Exception('Error logging in')
 
 
 # Start generating payload by appending all current classes
 htmlParser = Parser()
-htmlParser.parse(html)
+htmlParser.parse(responseStr)
 
-classPayload = settings['PAYLOAD_START'] + '&'.join(map(str, htmlParser.Classes))
-
+payloadStart = f'term_in={settings["SEMESTER"]}&RSTS_IN=DUMMY&assoc_term_in=DUMMY&CRN_IN=DUMMY&start_date_in=DUMMY&end_date_in=DUMMY&SUBJ=DUMMY&CRSE=DUMMY&SEC=DUMMY&LEVL=DUMMY&CRED=DUMMY&GMOD=DUMMY&TITLE=DUMMY&MESG=DUMMY&REG_BTN=DUMMY&MESG=DUMMY&'
+classPayload = payloadStart + '&'.join(map(str, htmlParser.Classes))
 
 # Finish payload by appending classes that need to be added
 RequestedCRNs = set(settings['REQUESTED_CRNs'].replace(' ', '').split(','))
@@ -59,33 +43,21 @@ CurrentCRNs = set(map(lambda c: c.data['CRN_IN'], htmlParser.Classes))
 CRNsToAdd = list(RequestedCRNs.difference(CurrentCRNs))
 
 regs_row = len(htmlParser.Classes)  # Current number of classes
-add_row = regs_row + len(CRNsToAdd)  # Total number of classes after adding new classes
+add_row = 1 + regs_row + len(CRNsToAdd)  # Total number of classes after adding new classes, 1 for the dummy class in payloadStart
 
 PaddedCRNsToAdd = CRNsToAdd + [''] * (19 - add_row)  # Pads list with empty CRNs, 19 is the expected number of classes
 for CRN in PaddedCRNsToAdd:
     classPayload += f'&RSTS_IN=RW&CRN_IN={CRN}&assoc_term_in=&start_date_in=&end_date_in='
 
 # Add final payload information, will need to change to include signing up for waitlists
-classPayload += f'regs_row={regs_row}&wait_row=0&add_row={add_row}&REG_BTN=Submit+Changes'
+classPayload += f'&regs_row={regs_row}&wait_row=0&add_row={add_row}&REG_BTN=Submit+Changes'
 
 
-req = s.prepare_request(requests.Request('POST', settings['BASE_URL'] + 'bwckcoms.P_Regs', data=classPayload))
-req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-req.headers['Referer'] = 'https://oscar.gatech.edu/pls/bprod/bwskfreg.P_AltPin'
-req.headers['Origin'] = 'https://oscar.gatech.edu'
-req.headers['Host'] = 'oscar.gatech.edu'
-req.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
-req.headers['Accept-Encoding'] = 'gzip, deflate, br'
-req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
-req.headers['Upgrade-Insecure-Requests'] = '1'
-
-req._cookies['TESTID'] = 'set'
-
+# Send finished payload
 print('Prepared post request to sign up for ' + ', '.join(CRNsToAdd))
-print(response.headers)
-print(req.headers)
-print(req._cookies)
+print(classPayload)
 if input('Send post request? (Y/N)').upper() == 'Y':
-    response = s.send(req)
+    send = s.post(settings['BASE_URL'] + 'bwckcoms.P_Regs', data=classPayload)
+
     print(response.status_code)
     print(response.content, file=open('response.html', 'w'))
---
+++ Waitlist.py
@@ -0,0 +1,71 @@
+import requests
+import time
+import winsound
+import webbrowser
+import clipboard
+
+
+requestedCRNs = ''
+requestInterval = 60
+
+baseURL = 'https://oscar.gatech.edu/pls/bprod/bwckschd.p_disp_detail_sched?term_in=202108&crn_in='
+
+
+def updateSeats():
+    for i, crn in enumerate(crns):
+        url = baseURL + crn
+        try:
+            html = str(s.get(url, timeout=2).content)
+
+            pos = [html.index("dddefault")]
+            seats = []
+            for posIndex in range(6):
+                pos.append(html.index("dddefault", pos[posIndex] + 1) + 11)
+                seats.append(int("".join([c for c in html[pos[-1]:pos[-1] + 3] if c.isdigit()])))
+
+            totalSeats = seats[0]
+            currentSeats = seats[1]
+            remainingSeats = seats[2]
+            waitList = seats[4]
+
+            if totalSeats < 1 or totalSeats != currentSeats + remainingSeats + waitList:
+                print('Invalid class CRN: ' + crn)
+
+            if remainingSeats > 0:
+                if not opened[i]:
+                    opened[i] = True
+
+                    winsound.Beep(2000, 500)
+                    clipboard.copy(crn)
+                    webbrowser.open(url, new=2)
+
+                print('Seat available in CRN: ' + crn + ' with ' + str(currentSeats) + ' out of ' + str(totalSeats))
+            else:
+                opened[i] = False
+        except Exception as e:
+            print('Error reaching ' + url + ' with error: ' + str(e))
+
+
+# Request classes
+s = requests.Session()
+s.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' \
+                          '(KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+
+crns = requestedCRNs.replace(' ', '').split(',')
+opened = [False] * len(crns)
+
+requestNumber = 0
+while True:
+    print('\nMaking Request ' + str(requestNumber))
+    requestNumber += 1
+    requestTime = time.time()
+    updateSeats()
+
+    if not any(opened):
+        print('No seats available in any classes')
+
+    sleepTime = requestInterval - (time.time() - requestTime)
+    if sleepTime > 0:
+        time.sleep(sleepTime)
+    else:
+        print('Requests taking longer than given time interval of ' + str(requestInterval))
